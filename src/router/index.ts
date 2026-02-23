30e2972bd6771953b6feb240a0f03137db93312a
Edward-Watts <edwardwadjei@gmail.com>
2026-02-23 10:03:57 +0000

    create login and register views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/login/LoginView.vue'
+import RegisterView from '@/views/register/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
     {
       path: '/about',

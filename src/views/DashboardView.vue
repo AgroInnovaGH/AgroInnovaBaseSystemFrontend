6e05af972193b9c5316201d99490d7ad8f1b880c
Kaycee <nduefunakingsley19@gmail.com>
2026-02-26 14:32:09 +0000

    adding footer branch

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <MyHeader />
+    <MyFooter />
   </div>
 </template>
 
 <script setup lang="ts">
 import MyHeader from '../components/header/MyHeader.vue';
-``
+import MyFooter from '../components/footer/MyFooter.vue';
+
 </script>
 
-<style scoped>
 
-</style>

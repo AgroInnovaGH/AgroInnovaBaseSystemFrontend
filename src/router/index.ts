15abf627c677c2904c92f67107add8f2bf23f230
Kaycee <nduefunakingsley19@gmail.com>
2026-02-26 04:35:45 +0000

    adding the header branch

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/login/LoginView.vue'
 import RegisterView from '@/views/register/RegisterView.vue'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'register',
       component: RegisterView,
     },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    }
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <MyHeader />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MyHeader from '../components/header/MyHeader.vue';
+``
+</script>
+
+<style scoped>
+
+</style>
